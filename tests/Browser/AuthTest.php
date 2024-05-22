4e7c5df1cbe344a9e6120b1a69d4f0cd20840bce
Dasun Tharanga <hello@dasun.dev>
2024-05-22 10:48:41 +0530

    wip

---
+++ tests/Browser/AuthTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Dasundev\TestbenchDuskDemo\Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Orchestra\Testbench\Attributes\WithMigration;
+use Orchestra\Testbench\Dusk\TestCase;
+use Orchestra\Testbench\Factories\UserFactory;
+use PHPUnit\Framework\Attributes\Test;
+
+class AuthTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    #[Test]
+    #[WithMigration]
+    public function can_authenticate_user()
+    {
+        $user = UserFactory::new()->create();
+
+        $this->browse(static fn (Browser $browser) => $browser
+            ->loginAs($user, 'web')
+            ->assertAuthenticatedAs($user, 'web')
+        );
+    }
+}
